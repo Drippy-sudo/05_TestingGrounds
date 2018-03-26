17ec466c5441b1ac8f0eb0591eab9318f752fb11
DESKTOP-BL45ABJ\Derpy <simeonnorris99@outlook.com>
2018-03-27 00:55:48 +0100

    get rid of me

--- README.md
+++
@@ -1,7 +0,0 @@
-# 05_TestingGrounds
-Testing grounds is a section from my c++ unreal course. 
-
-## Course information: 
-Here is the [link](https://www.udemy.com/course-dashboard-redirect/?course_id=657932) for the course.
-
-With the course, i'm creating a FPS game with a stealth aspect to it, which is inspired by Hunger Games.
---
+++ Source/S05_TestingGrounds/PatrollingGuard2.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "TP_ThirdPerson/TP_ThirdPersonCharacter.h"
+#include "PatrollingGuard2.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class S05_TESTINGGROUNDS_API APatrollingGuard2 : public ATP_ThirdPersonCharacter
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
